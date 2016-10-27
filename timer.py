d760fbbf6ecced9727efb800f8670c1e15948ef5
ALex Salyaev <dasparion@mail.ru>
2016-10-27 20:16:11 +0300

    устал писать это

--- timer.py
+++ timer.py
@@ -25,8 +25,10 @@ def tick():
     time_var.set(time.strftime(fmt, t))
     time_label.after(500, tick)  # следующий tick через 0.5 с
 
+
 def close_app():
     root.destroy()
+
 
 def timer():
     time_label = Label(f, textvariable=time_var, font="Courier 60",
@@ -34,15 +36,15 @@ def timer():
 
 
 menu = Menu(root)
-root.config(menu = menu)
+root.config(menu=menu)
 fm = Menu(menu)
-menu.add_cascade(label = 'File', menu = fm)
-fm.add_command(label = 'Exit', command = close_app)
+menu.add_cascade(label='File', menu=fm)
+fm.add_command(label='Exit', command=close_app)
 
 sm = Menu(menu)
-menu.add_cascade(label = 'Time', menu = sm)
-sm.add_command(label = '15', command = timer)
-sm.add_command(label = '30', command = timer)
+menu.add_cascade(label='Time', menu=sm)
+sm.add_command(label='15', command=timer)
+sm.add_command(label='30', command=timer)
 
 
 time_label.after(500, tick)
